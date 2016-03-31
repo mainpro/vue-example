727986240089ae9656110310d06f3fdab17bf04b
dipu <mainpro@126.com>
2016-03-31 14:24:17 +0800

    初始化项目

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,52 @@
 /**
  * Created by dipu on 2016/3/31.
  */
-var vue = require('vue-loader')
 var webpack = require('webpack')
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/main.js',
     output: {
         path: './dist',
         publicPath: '/dist/',
-        filename: 'app.js'
+        filename: 'build.js'
     },
     module: {
+        // avoid webpack trying to shim process
+        noParse: /es6-promise\.js$/,
         loaders: [
             {
                 test: /\.vue$/,
                 loader: 'vue'
+            },
+            {
+                test: /\.js$/,
+                // excluding some local linked packages.
+                // for normal use cases only node_modules is needed.
+                exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+                loader: 'babel'
             }
         ]
+    },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
     }
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ]
+} else {
+    module.exports.devtool = '#source-map'
 }
